cbff35ea9e911f2598b3f7adff1d5a32c2c22504
glisar <127492864+glisar@users.noreply.github.com>
2023-05-08 17:51:41 +0530

    21 video completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import logo from './logo.svg';
+
 import './App.css';
-// import About from './components/About';
+
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import About from './components/About';
+
 
 function App() {
 
@@ -31,20 +31,13 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#31323c";
       showAlert(" Dark mode has been enabled", "success");
-      // document.title = "TextUtils-Dark Mode";
-      // setInterval(()=>{
-      //   document.title = "TextUtils is Amazing Mode."
-      // },2000)
-
-      // setInterval(()=>{
-      //   document.title = "Install TextUtils Now."
-      // },1500)
+     
     }
     else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert(" Light mode has been enabled", "success");
-      // document.title = "TextUtils-Light Mode";
+      
 
     }
   }
@@ -52,7 +45,7 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar title="TextUtils" aboutText="about TextUtils" mode={mode} toggleMode={toggleMode} />
-        {/* <Navbar /> */}
+      
         <Alert alert={alert} />
 
         <div className="container my-4">
